c3a47a6f817069384ccba36ce6ce3ae506e35ef0
Noah Jeffers <njeffers@academic.rrc.ca>
2021-01-28 15:13:26 -0600

    modified routes for adventurers and linked to item

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   resources :items
+  resources :adventurers, only: %i[index show]
+
+  root to: "adventurers#index"
   # Domain Specific Language
   # all the routing syntax
-  get "/adventurers", to: "adventurers#index", as: "adventurers"
-  get "/adventurers/:id", to: "adventurers#show", as: "adventurer", constraints: { id: /\d+/ }
+  # get "/adventurers", to: "adventurers#index", as: "adventurers"
+  # get "/adventurers/:id", to: "adventurers#show", as: "adventurer", constraints: { id: /\d+/ }
 end
